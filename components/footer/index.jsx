7c91fbd551ddc3a538f7ae4777aeb77fcab629a3
AnvarovM <anvarovmuxammadxuja2001@gmail.com>
2022-09-28 00:09:49 +0500

    build: add footer layout

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import Image from 'next/image';
+import Logo from '../../public/assets/images/qalampir.png';
+import Link from 'next/link';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="container mx-auto flex items-center justify-between py-10 lg:py-16">
+        <p className="ml-5 lg:ml-32 text-lg font-medium text-[#001F00] dark:text-white">
+          ©2022
+        </p>
+        <ul className="flex items-center space-x-2 md:space-x-4 lg:space-x-6 xl:space-x-10 text-[#001F00] dark:text-white text-lg md:text-xl font-medium md:font-semibold">
+          <li>
+            <a target="_blank" href="google.com">
+              <p>IG</p>
+            </a>
+          </li>
+          <li>
+            <a target="_blank">
+              <p>TG</p>
+            </a>
+          </li>
+          <li>
+            <a target="_blank">
+              <p>IN</p>
+            </a>
+          </li>
+        </ul>
+        <p className="ml-5 lg:ml-32 text-lg font-medium text-[#001F00] dark:text-white">
+          M : hello@gmail.com
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- containers/Home.jsx
+++ containers/Home.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import { useTheme } from 'next-themes';
 import { useRouter } from 'next/router';
 
-import { Header, About } from '../components'
+import { Header, About } from '../components';
 
 export default function Home() {
   const [mounted, setMounted] = useState(false);
@@ -16,19 +16,20 @@ export default function Home() {
 
   if (!mounted) return null;
   return (
-    <div className='dark:bg-darkBG'>
+    <div className="">
       <Header />
       <section>
         <About />
       </section>
-      <main className='md:container md:mx-auto px-4'>
-      <h1 className="text-3xl font-bold underline">{locale}</h1>
-      <h1>The current theme is: {theme}</h1>
-      <div className="flex items-center space-x-4">
-        <button className='text-skin-main' onClick={() => setTheme('light')}>Light Mode</button>
-        <button onClick={() => setTheme('dark')}>Dark Mode</button>
-      </div>
-      <div className='h-screen bg-black'>s</div>
+      <main className="md:container md:mx-auto px-4">
+        <h1 className="text-3xl font-bold underline">{locale}</h1>
+        <h1>The current theme is: {theme}</h1>
+        <div className="flex items-center space-x-4">
+          <button className="text-skin-main" onClick={() => setTheme('light')}>
+            Light Mode
+          </button>
+          <button onClick={() => setTheme('dark')}>Dark Mode</button>
+        </div>
       </main>
     </div>
   );
